c4a807d94657a9f9515c9f0a984e988b5f006d84
VIKRAMASIMHAREDDY <VIKRAMASIMHAREDDY@DESKTOP-DFJH4HG>
2020-01-04 17:37:27 +0530

    firstpage

---
+++ FirstPage/src/main/java/com/vikram/dev/FirstPage/FirstPageApplication.java
@@ -0,0 +1,13 @@
+package com.vikram.dev.FirstPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPageApplication.class, args);
+	}
+
+}
---
+++ FirstPage/src/test/java/com/vikram/dev/FirstPage/FirstPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.dev.FirstPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
